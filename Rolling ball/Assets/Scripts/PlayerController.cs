ae802d2b5969fd7fa2612db0f64313aea600f8d6
Caleb Kirksey <ckirksey3@gmail.com>
2016-03-02 19:23:10 -0500

    added two ramps and a jump function

--- Rolling ball/Assets/Scripts/PlayerController.cs
+++ Rolling ball/Assets/Scripts/PlayerController.cs
@@ -9,18 +9,25 @@ public class PlayerController : MonoBehaviour {
 	public Text winText;
 	private Rigidbody rb;
 	private int count;
+	private bool grounded;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent <Rigidbody> ();
 		count = 0;
 		SetCountText ();
 		winText.text = "";
+		grounded = true;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
+		bool jump = Input.GetKey(KeyCode.Space);
+		if(jump && grounded) {
+			grounded = false;
+			rb.AddForce(new Vector3(0.0f, 200.0f, 0.0f));
+		}
 		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 		rb.AddForce (movement * speed);
 	}
@@ -46,10 +53,9 @@ public class PlayerController : MonoBehaviour {
 		if (other.gameObject.CompareTag ("speed boost")) {
 			other.gameObject.SetActive (false);
 			speed = 20;
-
-
-
-
+		}
+		if (other.gameObject.CompareTag("floor")) {
+			grounded = true;
 		}
 
 	}
